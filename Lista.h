b79e58dad59895f86b8f1d5130f84b761cd39093
KAUAN HENRIQUE KAESTNER <kauan.kaestner@catolicasc.edu.br>
2024-09-30 21:40:47 -0300

    lista funcionando

--- Lista.h
+++ Lista.h
@@ -16,22 +16,105 @@ private:
     }
 
     void removeAux(Lista *a){
-        a->anterior->prox = a->prox;
-        a->prox->anterior = a->anterior;
+    	
+    	if (this->length == 1){
+    		value = NULL;
+    		this->length = 0;
+    		return;
+		}
+    	
+    	this->length--;
+    	
+    	if (a->anterior != NULL){
+        	a->anterior->prox = a->prox;
+		}
+		if (a->prox != NULL){
+        	a->prox->anterior = a->anterior;
+		}
     }
+    
+    int partition(int low, int high){
+	
+		int pivo = this->get(low);
+		int j = low;
+		for (int i = low+1; i < high+1; i++){
+			
+			if (this->get(i) <= pivo){
+				j++;
+				int n1 = this->get(i);
+				int n2 = this->get(j);
+				this->changeValue(i, n2);
+				this->changeValue(j,n1);
+			}
+			
+		}
+		this->changeValue(low, this->get(j));
+		this->changeValue(j, pivo);
+		return j;
+	}
+    
+    void quickSort(int low, int high){
+    	
+    	if (low < high){
+			int p = partition(low, high);
+			quickSort(low, p-1);
+			quickSort(p+1, high);
+		}
+    	
+	}
 
 public:
-    int value;
+	
+    int value = NULL;
+    // bool valueVazio = true;
+    int length = 0;
 
     Lista(int value){
         this->value = value;
+        // this->valueVazio = false;
     }
 
 
+	int get(int index){
+		
+		Lista *aux = this;
+		int i = 0;
+		while (aux != NULL){
+			if (i == index) return aux->value;
+			aux = aux->prox;
+			i++;
+		}
+		printf("Erro: index %d fora de alcance\n", index);
+		
+	}
+	
+	void changeValue(int index, int value){
+		
+		Lista *aux = this;
+		int i = 0;
+		while (aux != NULL){
+			if (i == index){
+				aux->value = value;
+				break;
+			}
+			aux = aux->prox;
+			i++;
+		}
+		
+	}
+
     void append(int value){
+
+		this->length += 1;
+
+		if (this->value == NULL){
+        	this->value = value;
+        	return;
+		}
 
         Lista *novo = new Lista(value);
         Lista *ultimo = getUltimo();
+        
         ultimo->prox = novo;
         novo->anterior = ultimo;
         novo->prox = NULL;
@@ -61,9 +144,14 @@ public:
 
     void pop(int index){
 
+		if (index > this->length-1){
+			printf("Erro: index fora de alcance\n");
+			return;
+		}
+
         Lista *aux = this;
         int i = 0;
-        while (aux->prox != NULL){
+        while (aux != NULL){
             if (i == index){
                 removeAux(aux);
                 return;
@@ -75,8 +163,13 @@ public:
 
     }
 
+    // Remove o elemento com o valor especificado com o menor �ndice
     void remove(int value){
-        // Remove o elemento com o valor especificado com o menor índice
+
+		if (this->length == 0){
+			printf("Erro: Nao pode remover um elemento de uma lista vazia\n");
+			return;
+		}
 
         Lista *aux = this;
         while (aux->prox != NULL){
@@ -90,11 +183,20 @@ public:
 
     }
 
+    void sort(){
+        quickSort(0, this->length-1);
+    }
+
     void imprimir(){
+
+		if (this->length == 0){
+			printf("Lista vazia\n");
+			return;
+		}
 
         Lista *aux = this;
         while (aux != NULL){
-            printf("%d\n", aux->value);
+            printf("%d ", aux->value);
             aux = aux->prox;
         }
         printf("\n");
--- main.cpp
+++ main.cpp
@@ -2,22 +2,35 @@
 #include "Lista.h"
 
 
-
 int main(void){
 
-    Lista lista(1);
-    for (int i = 2; i < 10; i++){
-        lista.append(i);
-    }
+	Lista lista = NULL;
 
-    lista.pop(3);
-    lista.remove(8);
+	printf("Lista criada:\n");
+	for (int i = 10; i > 0; i--){
+		lista.append(i);
+	}
+	lista.imprimir();
 
-    lista.imprimir();
+	printf("Lista ordenada:\n");
+	lista.sort();
+	lista.imprimir();
 
-    lista.insert(4, 11);
-    lista.imprimir();
+	printf("Remover 8:\n");
+	lista.remove(8);
+	lista.imprimir();
 
+	printf("Trocar valor do index 2 por 11:\n");
+	lista.changeValue(2, 11);
+	lista.imprimir();
+
+	printf("Adicionar 12 no index 4:\n");
+	lista.insert(4, 12);
+	lista.imprimir();
+
+	printf("Remover elemento do index 5:\n");
+	lista.pop(5);
+	lista.imprimir();
 
     return 0;
 }
